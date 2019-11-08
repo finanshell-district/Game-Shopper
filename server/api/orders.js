4347818e919c07c933f08ea11a0ef2b5da0aff09
Moshe Boord <mcboord@gmail.com>
2019-11-08 12:18:50 -0500

    working

---
+++ server/api/orders.js
@@ -0,0 +1,20 @@
+const router = require('express').Router()
+const {Game, User, Order, OrderItem} = require('../db/models')
+module.exports = router
+
+router.post('/', async (req, res, next) => {
+  try {
+    const {email, items} = req.body
+    const user = await User.findOne({where: {email}})
+
+    const order = await Order.create({UserId: user.id, status: 'COMPLETE'})
+
+    items.forEach(item => {
+      OrderItem.create({OrderId: order.id, GameId: item.id, quantity: item.qty})
+    })
+
+    res.json(user)
+  } catch (err) {
+    next(err)
+  }
+})
